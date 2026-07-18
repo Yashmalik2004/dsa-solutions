9a7898535713bf33a870a269657b63ba42a11cd1
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-07-18 23:55:58 +0530

    todays potd very very easy on LC

---
+++ gcd.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxi=nums[nums.size()-1];
+        int mini=nums[0];
+
+        while(mini!=0){
+            int temp=mini;
+            mini=maxi%mini;
+            maxi=temp;
+        }
+        return maxi;
+    }
+};
