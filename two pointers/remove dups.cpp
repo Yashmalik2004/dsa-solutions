c63dbc50166e67d400107135e621e00fad30579e
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-08-01 22:44:09 +0530

    new

---
+++ two pointers/remove dups.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int>arr(n);
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  int c=1;
+  for(int i=1;i<n;i++){
+    if(arr[i]!=arr[i-1]){
+      arr[c++]=arr[i];
+    }
+  }
+  for(int i=0;i<c;i++){
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
