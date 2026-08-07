10fbf0d421f3daacea5adc94b3f6948c1453807c
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-08-07 23:51:23 +0530

    cooking

---
+++ BS/search in rotated sorted array.cpp
@@ -0,0 +1,14 @@
+#include<b1ts/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  vector<int> arr={7,8,9,1,2,3,4,5,6};
+  int n = arr.size();
+  int l=0,r=n-1;
+
+  while(l<r){
+    int m = l+(r-l)/2;
+  }
+
+}
