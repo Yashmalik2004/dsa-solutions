939b9a2722329dbe7f4adea571cabb252bc3495c
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-08-07 17:48:46 +0530

    new method for peak element updated

--- BS/peak elements.cpp
+++ BS/peak elements.cpp
@@ -9,18 +9,25 @@ int main(){
   while(l<=r){
     int m=l+(r-l)/2;
     
-    if(arr[m]>arr[m-1] && arr[m]>arr[m+1]){
-      if(m<(n-1) && m>0){
-        cout<<arr[m]<<endl;
-        break;
-      }
-    }
+    // if(arr[m]>arr[m-1] && arr[m]>arr[m+1]){
+    //   if(m<(n-1) && m>0){
+    //     cout<<arr[m]<<endl;
+    //     break;
+    //   }
+    // }
+    // if(arr[m]<arr[m+1]){
+    //   l=m+1;
+    // }
+    // else{
+    //   r=m-1;
+    // }
     if(arr[m]<arr[m+1]){
       l=m+1;
     }
     else{
-      r=m-1;
+      r=m;
     }
   }
+  return l;
   return 0;
 }
