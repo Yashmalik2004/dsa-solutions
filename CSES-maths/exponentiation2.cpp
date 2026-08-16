df6582274d6af7457e0ee132672ada9973f4ce72
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-08-16 14:42:25 +0530

    changes in expo 2

--- CSES-maths/exponentiation2.cpp
+++ CSES-maths/exponentiation2.cpp
@@ -8,11 +8,12 @@ long long power(long long a, long long b, long long mod) {
     long long ans = 1;
 
     while (b > 0) {
-        if (b & 1)
+        if (b % 2 == 1) {
             ans = (ans * a) % mod;
+        }
 
         a = (a * a) % mod;
-        b >>= 1;
+        b /= 2;
     }
 
     return ans;
@@ -29,17 +30,7 @@ int main() {
         long long a, b, c;
         cin >> a >> b >> c;
 
-        if (a == 0) {
-            if (b == 0 && c == 0)
-                cout << 1 << '\n';
-            else
-                cout << 0 << '\n';
-
-            continue;
-        }
-
         long long exponent = power(b, c, PHI);
-
         long long ans = power(a, exponent, MOD);
 
         cout << ans << '\n';
