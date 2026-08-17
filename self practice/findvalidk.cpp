fc7ef3dddd051ca992b7eb0437c1e887c9a9d2f6
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-08-18 00:00:16 +0530

    changes

--- self practice/findvalidk.cpp
+++ self practice/findvalidk.cpp
@@ -1,3 +1,47 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int T;
+//     cin >> T;
+
+//     while (T--) {
+//         int n;
+//         cin >> n;
+
+//         vector<int> w(n + 1);
+//         for (int i = 1; i <= n; i++)
+//             cin >> w[i];
+
+//         if (n % 2 == 1) {
+//             cout << "NO\n";
+//             continue;
+//         }
+
+//         int low = 0;
+//         int high = 1000000001;
+
+//         for (int i = 1; i <= n; i++) {
+//             if (i % 2 == 1) {
+//                 // odd positions move right => w[i] > k
+//                 high = min(high, w[i]);
+//             } else {
+//                 // even positions move left => w[i] < k
+//                 low = max(low, w[i]);
+//             }
+//         }
+
+//         if (low < high - 1)
+//             cout << "YES\n";
+//         else
+//             cout << "NO\n";
+//     }
+
+//     return 0;
+// }
 #include <bits/stdc++.h>
 using namespace std;
 
