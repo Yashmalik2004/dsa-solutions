56fa32425b35b4f63c252ce83249d61722cfdbcc
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-08-17 23:58:59 +0530

    new

--- CSES-maths/counting divisors.cpp
+++ CSES-maths/counting divisors.cpp
@@ -1,3 +1,33 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int n;
+//     cin >> n;
+
+//     while (n--) {
+//         int x;
+//         cin >> x;
+
+//         int count = 0;
+
+//         for (int i = 1; i * i <= x; i++) {
+//             if (x % i == 0) {
+//                 count += 2;
+
+//                 if (i * i == x)
+//                     count--;
+//             }
+//         }
+
+//         cout << count << '\n';
+//     }
+
+//     return 0;
+// }
 #include <bits/stdc++.h>
 using namespace std;
 
