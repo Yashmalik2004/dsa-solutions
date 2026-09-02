97cad33fd52e163f787e85fbbaf0c177f01a0260
Yash Malik <YOUR_GITHUB_EMAIL>
2026-09-02 18:51:02 +0530

    rec-3

--- recursion/self practice/patterns.cpp
+++ recursion/self practice/patterns.cpp
@@ -1,38 +1,131 @@
 
-// //print 1 to n using recursion
+// // //print 1 to n using recursion
+// // #include<bits/stdc++.h>
+// // using namespace std;
+
+// // void fnc(int i,int n){
+// //   if(i>n){
+// //     return;
+// //   }
+// //   cout<<i<<" ";
+// //   fnc(i+1,n);
+// // }
+
+// // int main(){
+// //   int n;
+// //   cin>>n;
+// //   fnc(1,n);
+// //   return 0;
+// // }
+
+// // print n to 1 using recursion
+
 // #include<bits/stdc++.h>
 // using namespace std;
 
-// void fnc(int i,int n){
-//   if(i>n){
+// void fnc(int n){
+//   if(n==0){
 //     return;
 //   }
-//   cout<<i<<" ";
-//   fnc(i+1,n);
+//   cout<<n<<" ";
+//   fnc(n-1);
 // }
-
 // int main(){
 //   int n;
 //   cin>>n;
-//   fnc(1,n);
+//   fnc(n);
 //   return 0;
 // }
 
-// print n to 1 using recursion
-
+// sum of n numbers using recursion
 #include<bits/stdc++.h>
 using namespace std;
 
-void fnc(int n){
-  if(n==0){
-    return;
-  }
-  cout<<n<<" ";
-  fnc(n-1);
+int fnc(int n,int sum){
+  if(n==0) return sum;
+  sum+=n;
+  fnc(n-1,sum);
 }
 int main(){
   int n;
   cin>>n;
-  fnc(n);
+  int sum = fnc(n,0);
+  cout<<sum;
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
