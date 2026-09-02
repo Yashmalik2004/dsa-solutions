b84e0c51bb8109eef510683489214a01c8bf12eb
Yash Malik <YOUR_GITHUB_EMAIL>
2026-09-02 18:43:12 +0530

    rec-2

--- recursion/self practice/patterns.cpp
+++ recursion/self practice/patterns.cpp
@@ -1,17 +1,38 @@
+
+// //print 1 to n using recursion
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void fnc(int i,int n){
+//   if(i>n){
+//     return;
+//   }
+//   cout<<i<<" ";
+//   fnc(i+1,n);
+// }
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   fnc(1,n);
+//   return 0;
+// }
+
+// print n to 1 using recursion
+
 #include<bits/stdc++.h>
 using namespace std;
 
-void fnc(int i,int n){
-  if(i>n){
+void fnc(int n){
+  if(n==0){
     return;
   }
-  cout<<i<<" ";
-  fnc(i+1,n);
+  cout<<n<<" ";
+  fnc(n-1);
 }
-
 int main(){
   int n;
   cin>>n;
-  fnc(1,n);
+  fnc(n);
   return 0;
 }
